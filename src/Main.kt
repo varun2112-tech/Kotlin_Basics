a266d762f9e6f0fc2dbc95381c0580ed069220d2
Varun <varunagarwal816@gmail.com>
2026-01-15 22:53:56 +0530

    Day 1: Kotlin basics (val,var, assignment operators)

---
+++ src/Main.kt
@@ -0,0 +1,29 @@
+
+
+fun main() {
+    var myVariable: Int = 5
+    myVariable = 1
+    println("The value of our variable is $myVariable")
+
+    val myVariable2 = 3
+    println(myVariable2)
+
+    var myVariable3: Boolean = true
+    println(myVariable3)
+
+    // operators
+
+        val x: Float = 15F
+        val y: Float  = 7F
+        val result = x+y
+        println(result)
+
+    var a = 20
+    var b = 8
+    println("The rsult of a-b is ${a - b}")
+    a += 10
+    println("The result of a-b is now ${a - b}")
+
+    //Decimals
+    println("the result of x/y is ${x/y}")
+}
